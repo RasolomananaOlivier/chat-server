8e5a0f6c39fa7a199f2958ebbdf4a70653de56e6
Olivier-rh <rasolomananaherimanitraolivier@gmail.com>
2021-10-28 06:26:02 +0300

    databse

--- database/config.js
+++ database/config.js
@@ -1,3 +1,2 @@
-//exports.database = "mongodb+srv://Olivier:Herimanitra0@cluster0.6kowo.mongodb.net/chatapp?retryWrites=true&w=majority";
+exports.database = "mongodb+srv://Olivier:Herimanitra0@cluster0.6kowo.mongodb.net/chatapp?retryWrites=true&w=majority";
 
-exports.database = 'mongodb://127.0.0.1:27017/chatapp'
