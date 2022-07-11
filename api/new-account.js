92bb8341b2a36dd0a05d67fa069921ede4a2ce58
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2022-07-11 18:45:52 +0300

    add delete notification route

--- api/new-account.js
+++ api/new-account.js
@@ -10,44 +10,44 @@ const app = express()
 
 app.post("/signup", async (req, res) => {
 
-  try {
+    try {
 
-    const { firstName, lastName, email, birthday, bio, passwd, avatarFileName } = req.body
-
-    // Find if the user email already exist 
-    const emailTaken = await User.findOne({ email: email })
-
-
-    bcrypt.hash(passwd, 10, async function (err, hashedPassword) {
-      err && res.json({ err: "Error while hashing password" })
+        const { firstName, lastName, email, birthday, bio, passwd, avatarFileName } = req.body
+        // Find if the user email already exist
+        const emailTaken = await User.findOne({ email: email })
 
 
-      const newUser = new User({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        birthday: birthday,
-        bio: bio,
-        avatarFileName: avatarFileName ?? null,
-        passwd: hashedPassword,
-        requests: [],
-        friendsCollections: [],
-        notificationsCollections: []
-      })
-      const saved = await newUser.save()
-      const token = jwt.sign(JSON.stringify(saved), process.env.TOKEN_SECRET);
-      result = {
-        user: saved,
+        bcrypt.hash(passwd, 10, async function (err, hashedPassword) {
+            err && res.json({ err: "Error while hashing password" })
 
-        token: `bearer ${token}`,
-        isRegistered: true,
-      }
 
-      res.json(result)
-    })
-  } catch (error) {
-    res.json({ err: "Error while trying to save data" + error, isRegistered: false })
-  }
+            const newUser = new User({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                birthday: birthday ?? null,
+                bio: bio,
+                avatarFileName: avatarFileName ?? null,
+                passwd: hashedPassword,
+                requests: [],
+                friendsCollections: [],
+                notificationsCollections: []
+            })
+            const saved = await newUser.save()
+            const token = jwt.sign(JSON.stringify(saved), process.env.TOKEN_SECRET);
+            result = {
+                user: saved,
+
+                token: `bearer ${token}`,
+                isRegistered: true,
+            }
+
+            res.json(result)
+        })
+
+    } catch (error) {
+        res.json({ err: "Error while trying to save data" + error, isRegistered: false })
+    }
 });
 
 
--- server.js
+++ server.js
@@ -10,18 +10,14 @@ const RouterLogin = require("./api/authentification");
 const RouterUpload = require("./api/upload-avatar");
 const RouterUser = require("./api/all-users");
 const MessageRouter = require("./api/message");
+const NotificationRouter = require('./api/notification')
 
 const cors = require("cors");
-const sort = require("./controllers/sortOnline");
 const sortOffline = require("./controllers/sortOffline");
 const Message = require("./models/message-model");
 const createNewMessage = require("./middleware/createNewMessage");
-const sendToMe = require("./middleware/sendToMe");
-const sendToFriend = require("./middleware/sendtofriend");
 const User = require("./models/user-model");
 const { AddFriendAndRemoveRequest, AddMeToFriendCollection } = require("./middleware/addFriendAndRemoveNot");
-const addNotification = require("./middleware/addNotification");
-const addFriendAndRemoveNot = require("./middleware/addFriendAndRemoveNot");
 const addNotificationToFriend = require("./middleware/addNotification");
 const createNewMedia = require("./middleware/createNewMedia");
 const Media = require("./models/media-model");
@@ -29,10 +25,10 @@ const { DATABASE } = require("./database/config");
 
 
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+    },
 });
 
 app.use(express.json());
@@ -42,127 +38,127 @@ app.use(cors({ origin: "*" }));
 //const db ="mongodb+srv://Olivier:Herimanitra0@cluster0.6kowo.mongodb.net/chatapp?retryWrites=true&w=majority";
 
 mongoose
-  .connect(DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.info("> Mongodb connected");
-  })
-  .catch(() => {
-    console.log("> Failed to connected to mongodb");
-  });
+    .connect(DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.info("> Mongodb connected");
+    })
+    .catch(() => {
+        console.log("> Failed to connected to mongodb");
+    });
 
 app.get("/", (req, res) => {
-  res.send({ message: "hello from the server" });
+    res.send({ message: "hello from the server" });
 });
 
 app.use('/pic', RouterUpload);
 
-app.use(RouterLogin, Router, RouterUser, MessageRouter);
+app.use(RouterLogin, Router, RouterUser, MessageRouter, NotificationRouter);
 
 let actif_array = [];
 
 io.on("connection", (socket) => {
-  console.log('One user connected', socket.id);
-  socket.on('test', (arg) => {
-    console.log('received test');
-    socket.emit('hello', '0007');
-  })
+    console.log('One user connected', socket.id);
+    socket.on('test', (arg) => {
+        console.log('received test');
+        socket.emit('hello', '0007');
+    })
 
 
-  socket.on("USER-CONNECTED", (user) => {
-    console.log(" > connected ; ", user);
-    actif_array.push(user);
+    socket.on("USER-CONNECTED", (user) => {
+        console.log(" > connected ; ", user);
+        actif_array.push(user);
 
-    // console.log(" > all connected ; ", actif_array);
+        // console.log(" > all connected ; ", actif_array);
 
-    io.emit("ONLINE_USERS", [... new Set(actif_array)]);
-  });
+        io.emit("ONLINE_USERS", [... new Set(actif_array)]);
+    });
 
-  socket.on("LOG_OUT", (user) => {
-    // console.log(" > disconnected ; " + user);
-    // console.log(" > disconnected arr : ", sortOffline(actif_array, user));
-    actif_array = sortOffline(actif_array, user);
-    io.emit("OFFLINE_USERS", actif_array);
-  });
+    socket.on("LOG_OUT", (user) => {
+        // console.log(" > disconnected ; " + user);
+        // console.log(" > disconnected arr : ", sortOffline(actif_array, user));
+        actif_array = sortOffline(actif_array, user);
+        io.emit("OFFLINE_USERS", actif_array);
+    });
 
-  socket.on("SEND_MESSAGE", async (data, item) => {
-    try {
+    socket.on("SEND_MESSAGE", async (data, item) => {
+        try {
 
-      const { userId, friendId, messagesId, mediasId } = data;
+            const { userId, friendId, messagesId, mediasId } = data;
 
 
-      const message = await Message.findById(messagesId);
+            const message = await Message.findById(messagesId);
 
-      message.items.push(item);
-      const savedMessage = await message.save();
+            message.items.push(item);
+            const savedMessage = await message.save();
 
-      let mediaSaved;
-      if (item.messageType === 'media') {
-        // console.log('new media', mediasId);
-        const mediaDoc = await Media.findById(mediasId);
-        const mediaId = {
-          mediaId: item.mediaId
+            let mediaSaved;
+            if (item.messageType === 'media') {
+                // console.log('new media', mediasId);
+                const mediaDoc = await Media.findById(mediasId);
+                const mediaId = {
+                    mediaId: item.mediaId
+                }
+                mediaDoc.collections.push(mediaId);
+                mediaSaved = await mediaDoc.save();
+            }
+            // console.log('> media saved', mediaSaved);
+
+
+            socket.emit(`${userId}_NEW_MESSAGE`, savedMessage, mediaSaved);
+            io.emit(`${friendId}_NEW_MESSAGE`, savedMessage, mediaSaved);
+        } catch (error) {
+            console.log(error);
         }
-        mediaDoc.collections.push(mediaId);
-        mediaSaved = await mediaDoc.save();
-      }
-      // console.log('> media saved', mediaSaved);
 
-
-      socket.emit(`${userId}_NEW_MESSAGE`, savedMessage, mediaSaved);
-      io.emit(`${friendId}_NEW_MESSAGE`, savedMessage, mediaSaved);
-    } catch (error) {
-      console.log(error);
-    }
-
-  });
+    });
 
 
 
-  socket.on('SEND_REQUEST', async (data) => {
-    // console.log('Request received');
-    //  _id is id of the receiver
-    const { _id, details } = data;
-    const friendId = _id;
-    const result = await User.findOne({ _id: friendId });
+    socket.on('SEND_REQUEST', async (data) => {
+        console.log('Request received');
+        //  _id is id of the receiver
+        const { _id, details } = data;
+        const friendId = _id;
+        const result = await User.findOne({ _id: friendId });
 
-    result.requests.push(details);
-    const saved = await result.save();
-    // console.log('friendId', friendId);
-    io.emit(`${friendId}_NEW_REQUEST`, saved.requests);
-    // Verify if the 2 persons are already friends
+        result.requests.push(details);
+        const saved = await result.save();
+        // console.log('friendId', friendId);
+        io.emit(`${friendId}_NEW_REQUEST`, saved.requests);
+        // Verify if the 2 persons are already friends
 
-  });
+    });
 
-  /**
-   * Find the account of the user that accept the request
-   * Then, push the new friend details to his/her friend collections,
-   * Send the new collection to his propertary.
-   * Finally, send a notification to the other user that his/her
-   * request has been accepted.
-   */
-  socket.on('ACCEPT_REQUEST', async (data) => {
-    // console.log(data);
-    //  _id is id of the receiver
-    const { _id, details } = data;
+    /**
+     * Find the account of the user that accept the request
+     * Then, push the new friend details to his/her friend collections,
+     * Send the new collection to his propertary.
+     * Finally, send a notification to the other user that his/her
+     * request has been accepted.
+     */
+    socket.on('ACCEPT_REQUEST', async (data) => {
+        // console.log(data);
+        //  _id is id of the receiver
+        const { _id, details } = data;
 
-    const newMessageCollection = await createNewMessage(_id, details._id);
-    const newMediaCollection = await createNewMedia(_id, details._id);
+        const newMessageCollection = await createNewMessage(_id, details._id);
+        const newMediaCollection = await createNewMedia(_id, details._id);
 
-    const saved = await AddFriendAndRemoveRequest(_id, details);
-    // console.log('saved 1', saved.friendsCollections);
-    socket.emit(`${_id}_NEW_FRIEND_ACCEPTED`, saved, newMessageCollection, newMediaCollection);
+        const saved = await AddFriendAndRemoveRequest(_id, details);
+        // console.log('saved 1', saved.friendsCollections);
+        socket.emit(`${_id}_NEW_FRIEND_ACCEPTED`, saved, newMessageCollection, newMediaCollection);
 
-    await AddMeToFriendCollection(_id, details);
-    const notifUpdated = await addNotificationToFriend(_id, details);
-    // console.log('on Notify', notifUpdated);
-    io.emit(`${notifUpdated._id}_NEW_NOTIFICATION`, notifUpdated, newMessageCollection, newMediaCollection);
-    // console.log('Notification sent');
+        await AddMeToFriendCollection(_id, details);
+        const notifUpdated = await addNotificationToFriend(_id, details);
+        // console.log('on Notify', notifUpdated);
+        io.emit(`${notifUpdated._id}_NEW_NOTIFICATION`, notifUpdated, newMessageCollection, newMediaCollection);
+        // console.log('Notification sent');
 
-  });
+    });
 
 
 });
