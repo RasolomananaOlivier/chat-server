d14feb56e16b89248f4690c22bad00a64c846a89
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-02-21 08:22:43 +0300

    come one

--- dist/src/events/emiters/newRequest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.emitNewRequest = void 0;
-const app_1 = require("../../app");
-const emitNewRequest = (userId, request) => {
-    app_1.serverSocket.emit(`request:new:${userId}`, request);
-};
-exports.emitNewRequest = emitNewRequest;
